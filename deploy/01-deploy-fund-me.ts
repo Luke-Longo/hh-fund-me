562d36e483d658efdb84a51427d7ea0fc728de64
Luke-Longo <lukelongo0421@gmail.com>
2022-09-20 09:46:55 -0400

    cleaned up the priceFeed

--- deploy/01-deploy-fund-me.ts
+++ deploy/01-deploy-fund-me.ts
@@ -5,4 +5,6 @@ export default async (hre: HardhatRuntimeEnvironment) => {
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
     const chainId: number = network.config.chainId;
+
+    // when going for local host or hardhat network we want ot use a mock
 };
