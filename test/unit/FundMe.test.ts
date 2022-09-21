028cf111ce6bf35c5ee7c7b36d4f3cf25cae62e2
Luke-Longo <lukelongo0421@gmail.com>
2022-09-21 12:51:47 -0400

    commented each line for what is being done

--- test/unit/FundMe.test.ts
+++ test/unit/FundMe.test.ts
@@ -7,11 +7,16 @@ describe("FundMe", async function () {
     let deployer;
     let mockV3Aggregator: MockV3Aggregator;
     beforeEach(async () => {
+        // gets the accounts from the hardhat network
         const accounts = await ethers.getSigners();
         const accountZero = accounts[0];
+        // get the deployer account
         deployer = (await getNamedAccounts()).deployer;
+        // deploys the contract
         await deployments.fixture("all");
+        // gets the deployed contract instance to interact with
         fundMe = await ethers.getContract("FundMe", deployer);
+        // gets the deployed mockV3Aggregator instance to interact with
         mockV3Aggregator = await ethers.getContract(
             "MockV3Aggregator",
             deployer
@@ -19,6 +24,7 @@ describe("FundMe", async function () {
     });
     describe("Constructor", async function () {
         it("sets the aggregator addresses correctly", async function () {
+            // this function checks the price feed address and makes sure that the address is the same as the moc v3 deploye on the local hardahat network
             const response = await fundMe.priceFeed();
             assert.equal(response, mockV3Aggregator.address);
         });
