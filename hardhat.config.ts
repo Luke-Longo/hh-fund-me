667a1d067f3d64af8ad413a70e74606734c8a0b3
Luke-Longo <lukelongo0421@gmail.com>
2022-09-22 13:15:55 -0400

    update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -14,41 +14,41 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "";
 
 const config: HardhatUserConfig = {
-    defaultNetwork: "hardhat",
-    networks: {
-        goerli: {
-            url: ALCHEMY_GOERLI_URL,
-            accounts: [ALCHEMY_PRIVATE_KEY!],
-            chainId: 5,
-        },
-        localhost: {
-            url: "http://127.0.0.1:8545/",
-            chainId: 31337,
-        },
+  defaultNetwork: "hardhat",
+  networks: {
+    goerli: {
+      url: ALCHEMY_GOERLI_URL,
+      accounts: [ALCHEMY_PRIVATE_KEY!],
+      chainId: 5,
     },
-    // solidity: "0.8.8",
-    solidity: {
-        compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      chainId: 31337,
     },
-    etherscan: {
-        apiKey: ETHERSCAN_API_KEY,
+  },
+  // solidity: "0.8.8",
+  solidity: {
+    compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    token: "ETH",
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
     },
-    gasReporter: {
-        enabled: false,
-        currency: "USD",
-        outputFile: "gas-report.txt",
-        noColors: true,
-        coinmarketcap: COINMARKETCAP_API_KEY,
-        token: "ETH",
+    user: {
+      default: 1,
     },
-    namedAccounts: {
-        deployer: {
-            default: 0,
-        },
-        user: {
-            default: 1,
-        },
-    },
+  },
 };
 
 export default config;
